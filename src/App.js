a9b82108666b36cfa4ce6061834d26bacd247af8
Vedpotdar <vedantpotdargit9988@gmail.com>
2024-07-07 21:31:08 +0530

    commit ten

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import React from 'react';
 import AboutMePage from './component/AboutMePage';
 import Home from './component/Home'
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import PortfolioPage from './component/PortfolioPage';
 import ContactPage from './component/contactpage';
 import ServicesPage from './component/ServicesPage';
 
 function App() {
   return (
-    <Router>
-    <Switch>
-      <Route path='/my_portfolioweb' component={<Home/>}></Route>
-      <Route path='/home' component={<Home/>}></Route>
-      <Route path='/about' component={<AboutMePage/>}></Route>
-      <Route path='/portfolio' component={<PortfolioPage/>}></Route>
-      <Route path='/contact' component={<ContactPage/>}></Route>
-      <Route path='/services' component={<ServicesPage/>}></Route>
-    </Switch>
-    </Router>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>}></Route>
+      <Route path='/home' element={<Home/>}></Route>
+      <Route path='/about' element={<AboutMePage/>}></Route>
+      <Route path='/portfolio' element={<PortfolioPage/>}></Route>
+      <Route path='/contact' element={<ContactPage/>}></Route>
+      <Route path='/services' element={<ServicesPage/>}></Route>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
