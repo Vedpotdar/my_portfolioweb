d799beda3c3dc94d8666fe239f488842e8536446
Vedpotdar <vedantpotdargit9988@gmail.com>
2024-07-07 21:43:59 +0530

    Switch to HashRouter for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import AboutMePage from './component/AboutMePage';
-import Home from './component/Home'
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Home from './component/Home';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import PortfolioPage from './component/PortfolioPage';
 import ContactPage from './component/contactpage';
 import ServicesPage from './component/ServicesPage';
 
 function App() {
   return (
-    <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Home/>}></Route>
-      <Route path='/home' element={<Home/>}></Route>
-      <Route path='/about' element={<AboutMePage/>}></Route>
-      <Route path='/portfolio' element={<PortfolioPage/>}></Route>
-      <Route path='/contact' element={<ContactPage/>}></Route>
-      <Route path='/services' element={<ServicesPage/>}></Route>
-    </Routes>
-    </BrowserRouter>
+    <HashRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/about' element={<AboutMePage />} />
+        <Route path='/portfolio' element={<PortfolioPage />} />
+        <Route path='/contact' element={<ContactPage />} />
+        <Route path='/services' element={<ServicesPage />} />
+      </Routes>
+    </HashRouter>
   );
 }
 
 export default App;
+
