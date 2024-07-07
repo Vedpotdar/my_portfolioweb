0ffbbb83dd606861e5890861053aaecec7609823
Vedpotdar <vedantpotdargit9988@gmail.com>
2024-07-07 20:08:05 +0530

    add main path in App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <BrowserRouter>
     <Routes>
-      <Route path='/' element={<Home/>}></Route>
+      <Route path='/my_portfolioweb' element={<Home/>}></Route>
       <Route path='/home' element={<Home/>}></Route>
       <Route path='/about' element={<AboutMePage/>}></Route>
       <Route path='/portfolio' element={<PortfolioPage/>}></Route>
