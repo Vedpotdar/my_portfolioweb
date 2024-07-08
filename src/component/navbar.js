22a951fddf8282fe4a1dad6c69e5ce4be83219c9
Vedpotdar <vedantpotdargit9988@gmail.com>
2024-07-08 14:21:59 +0530

    Switch to HashRouter for GitHub Pages

--- src/component/navbar.js
+++ src/component/navbar.js
@@ -8,15 +8,15 @@ function OurnavBar() {
     <>
       <Navbar expand="lg" fixed="top" className="custom-navbar">
         <Container>
-          <Navbar.Brand href="home"><img src={LOGO} alt="Logo" /></Navbar.Brand>
+          <Navbar.Brand href="#home"><img src={LOGO} alt="Logo" /></Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto menu">
-              <Nav.Link className='item' href="home">Home</Nav.Link>
-              <Nav.Link className='item' href="about">About</Nav.Link>
-              <Nav.Link className='item' href="services">Services</Nav.Link>
-              <Nav.Link className='item' href="portfolio">Portfolio</Nav.Link>
-              <Nav.Link className='item' href="contact">Contact</Nav.Link>
+              <Nav.Link className='item' href="#home">Home</Nav.Link>
+              <Nav.Link className='item' href="#about">About</Nav.Link>
+              <Nav.Link className='item' href="#services">Services</Nav.Link>
+              <Nav.Link className='item' href="#portfolio">Portfolio</Nav.Link>
+              <Nav.Link className='item' href="#contact">Contact</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
