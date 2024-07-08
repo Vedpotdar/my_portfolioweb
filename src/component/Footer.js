eff011a0e302ab49576b706cc27e6e761304d005
Vedpotdar <vedantpotdargit9988@gmail.com>
2024-07-08 14:30:08 +0530

    Footer Update With #

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -16,11 +16,11 @@ function Footer() {
           <Col md={4} className="footer-section">
             <h5>Links</h5>
             <ul className="footer-links">
-              <li><a href="home">Home</a></li>
-              <li><a href="about">About</a></li>
-              <li><a href="services">Services</a></li>
-              <li><a href="portfolio">Portfolio</a></li>
-              <li><a href="contact">Contact</a></li>
+              <li><a href="#home">Home</a></li>
+              <li><a href="#about">About</a></li>
+              <li><a href="#services">Services</a></li>
+              <li><a href="#portfolio">Portfolio</a></li>
+              <li><a href="#contact">Contact</a></li>
             </ul>
           </Col>
           <Col md={4} className="footer-section">
